d59ff0e04af482f9be3d982aa4a3500834878f31
Joel Järvinen <joel.jarvinen@gmail.com>
2013-10-22 16:36:04 +0200

    New style for map and location pins

--- js/main.js
+++ js/main.js
@@ -1,54 +1,116 @@
 
-$(document).ready(function(){
+$(document).ready(function()
+{
 
-     markers = [];
-  var infowindow = null;
+var styles = [
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [
+      { hue: '#596B8A' },
+      { saturation: -75 },
+      { lightness: -20 },
+      { visibility: 'simplified' }
+    ]
+  },{
+    featureType: 'landscape',
+    elementType: 'geometry',
+    stylers: [
+      { hue: '#D9D9D9' },
+      { saturation: -100 },
+      { lightness: -4 },
+      { visibility: 'on' }
+    ]
+  },{
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      { hue: '#B8B8B8' },
+      { saturation: -100 },
+      { lightness: 23 },
+      { visibility: 'simplified' }
+    ]
+  },{
+    featureType: 'poi',
+    elementType: 'all',
+    stylers: [
+      { hue: '#D9D9D9' },
+      { saturation: -100 },
+      { lightness: 32 },
+      { visibility: 'off' }
+    ]
+  },{
+    featureType: 'transit',
+    elementType: 'all',
+    stylers: [
+      { hue: '#D9D9D9' },
+      { saturation: 0 },
+      { lightness: 40 },
+      { visibility: 'off' }
+    ]
+  }
+];
 
-    var myLatlng = new google.maps.LatLng(59.32, 18.06);
-    var mapOptions = {
+var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+
+markers = [];
+
+
+var iconBase = 'img/';
+  
+var infowindow = null;
+
+var myLatlng = new google.maps.LatLng(59.32, 18.06);
+
+var mapOptions = 
+    {
       zoom: 12,
       center: myLatlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeControlOptions: 
+    {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map']
     }
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    
+    }
 
-   var mapOptions = {
-           zoom: 9,
-           center: new google.maps.LatLng(28.9285745, 77.09149350000007),  
-           mapTypeId: google.maps.MapTypeId.TERRAIN
-       }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);  
+  map.mapTypes.set('map', styledMap);
+  map.setMapTypeId('map');    
 
-  function resize (){                        
+function resize (){                        
   google.maps.event.addDomListener(window, 'resize', initialize);
   google.maps.event.addDomListener(window, 'load', initialize);
   }
 
-    function createMarker(attributes) {
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(attributes.lat, attributes.lng),
-        map: map,
-        title: 'Hello World!'
-      });
+function createMarker(attributes) 
+{
+    var marker = new google.maps.Marker
+    ({
+      position: new google.maps.LatLng(attributes.lat, attributes.lng),
+      map: map,
+      icon: iconBase + 'caution.png',
+    });
 
 
-      google.maps.event.addListener(marker, 'click', function() {
-        map.setZoom(17);
-        if (infowindow) {
+      google.maps.event.addListener(marker, 'click', function()
+      {
+        map.setZoom(15);
+        if (infowindow) 
+      {
         infowindow.close();
-        }
+      }
 
-        var contentString = "<p>" + attributes.text + "<br><img class='pic' src='" + attributes.media + "'><br>Created at " + attributes.created_at;"</p>"
-        infowindow = new google.maps.InfoWindow({
+        var contentString = "<div class='infowindow'><p>" + "<br><img class='pic' src='" + attributes.media + "'><br>" + attributes.text + "<br>Created at " + attributes.created_at;"</p></div>"
+        infowindow = new google.maps.InfoWindow
+      ({
           content: contentString
-        });
+      });
         infowindow.open(map,marker);
       });
 
-
       marker.created_at = new Date(attributes.created_at).getTime();
       markers.push(marker);
-    }
+}
 
     var url = "https://free-ec2.scraperwiki.com/b732xeq/738b546f51cf436/sql/?q=select%20%0A%09created_at%2C%0A%20%20%20%20text%2C%0A%20%20%20%20lat%2C%0A%20%20%20%20lng%2C%0A%20%20%20%20media%0Afrom%20tweets%0Agroup%20by%20created_at%0A";
     
@@ -59,8 +121,9 @@ $(document).ready(function(){
           createMarker(point);
         }
         }
-     });
 });
+    });
+
 
 
 
